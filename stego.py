929db75f8890729dc97120891ba3b6116649db1e
adam-good <dev.adamgood@gmail.com>
2016-12-28 20:13:04 -0700

    Update stego.py
    All around update to almost everything. Updated User Interface as well as enabled it to use all the colors in a pixel instead of just red.

--- stego.py
+++ stego.py
@@ -1,140 +1,200 @@
-'''
-*       Name:   stego.py
-*       Author: Adam Good
+"""
+*       Name:       stego.py
+*       Author:     Adam Good
+*       Updated:    12-28-16
 *
 *       Description:
 *           Just a simple steganography script. It currently can hide a 
-*           message (from a file) into the red values of the pixels of an
-*           image. Just some afternoon fun I had. Will improve later.
+*           message (from a file) into the pixels of an image. Will improve later.
 *
 *       TODO:
-*           Store message in every color of pixel, not just red
-*           Better UI
 *           Clean the code
 *           Make it able to store any stort of payload, not just text
-'''
+*           Update comments so that they're easier to read and understand
+"""
 
+import sys
+import os
+import platform
 from PIL import Image
 
-# Convert the message to binary
-def getBits(msg):
+
+def get_bits(msg):
     bits = []
-    bytes = (ord(b) for b in msg) # Get the decimal value of each byte
-    for b in bytes:
+    bytes = (ord(byte) for byte in msg) # Get the decimal value of each byte
+    for bit in bytes:
         for i in xrange(8):
-            bits.append((b >> i) & 1) # Append the bits to the list of bits (might describe this more in depth later for those who don't understand bitwise operations)
+            # Append the bits to the list of bits.
+            bits.append((bit >> i) & 1)
     return bits
 
-def hideMsg(img, msg):
-    # img should already be open. Load the pixels and get the dimensions
-    pxls = img.load()
+
+def hide_msg(img, msg, out):
+    # Load the pixels and get the dimensions of the image.
+    pixels = img.load()
     rows,cols = img.size
 
-    # Ensure the length of the message is suitable
-    l = len(msg)
-    # If the message length can't be stored in the first row
-    if (l*8 >= 2**rows):
+    # Ensure the length of the message is suitable.
+    # We quit if we can't store the length in the first column or the total image is too small
+    length = len(msg)
+    if (length*8 >= 2**cols):
         print ("[X] Message so big we can't even store the length of it correctly...")
-    # If the message has more bits than the image has pixels
-    if (l*8 > rows*cols):
+        return None
+    if (length*8 > int((rows*cols) / 3)):
         print ("[X] Message to big for the image! Sorry!!")
         return None
     
-    # Convert the length to bits
+    # Convert the length to a list of bits that is the size of one collumn.
+    # The default bit value is 0.
     bits = [0] * rows
     for i in xrange(rows):
-        bits[i] = (l >> i) & 1
+        bits[i] = (length >> i) & 1
     
-    # Store the bits in the first row of the image    
+    # Store the bits in red color of the first column of the image.
+    # One entire column should be pleanty of space for this value on most standard images.
+    # TODO: find a better way to do this later.
     for x in xrange(rows):
-        r,g,b = pxls[x,0]
+        r,g,b = pixels[x,0]
         if (r%2 != bits[x]):
             r += 1
-        pxls[x,0] = (r,g,b)
+        pixels[x,0] = (r,g,b)
     
-    # Get the bits of the actual message. Also Initialize the coordinate variables we'll use
-    # Remember we start y at 1 because y=0 is where we stored the message length
-    bits = getBits(msg)
-    x = 0
-    y = 1
-    # Loop through all of our bits
-    for bit in bits:
-        # Edit the pixels so that they contain our message
-        r,g,b = pxls[x,y]   # get the pixel values
-        if (bit != (r%2)):  # If the least significant bit is not correct we flip it
-            r += 1
-        pxls[x,y] = (r,g,b) # Assign the new value to our pixel
-        
-        # Increment our coordinates in the correct order
-        x += 1
-        if (x >= rows):
-            x = 0
-            y += 1
+    # Get the bits of the actual message.
+    # Loop through the image storing the bits as we go
+    bits = get_bits(msg)
+    i = 0   # Index for bits
+    for y in xrange(1,rows):
+        for x in xrange(cols):
+            r,g,b = pixels[x,y]   # Get the values from pixel
+            try:
+                # Store three bits at a time into r,g,b respectivly
+                r += (1 if (r%2 != bits[i]) else 0)
+                g += (1 if (g%2 != bits[i+1]) else 0)
+                b += (1 if (b%2 != bits[i+2]) else 0)
+                # Update the actual pixel with our new values as well as increment our bit counter
+                pixels[x,y] = (r,g,b)
+                i += 3
+            # If we run out of bits we'll get an IndexError when we try to update the vale. That's when we know we're done    
+            # It would probably be better to do this without having to catch the error but this works for now
+            # TODO: Better way to do this
+            except IndexError:
+                # Set the final pixel and save the image
+                pixels[x,y] = (r,g,b)
+                img.save(out)
+                return
 
-    # Save the new image and notify the user
-    secretImage = "img_msg.png" 
-    img.save(secretImage)
-    print "[*] Message hidden in " + secretImage
-    
-# This will get a message from an image
-def readMsg(img):
-    # Load the pixels and get the dimensions
-    pxls = img.load()
+
+def read_msg(img):
+    # Load the pixels and get the dimensions.
+    pixels = img.load()
     rows,cols = img.size
 
-    # First we get the length of the message stored in the first row
+    # First we get the length of the message stored in the first column.
     length = 0
     for i in range(rows):
-        r,g,b = pxls[i,0]
+        r,g,b = pixels[i,0]
         if (r%2 == 1):
-            length += 2**i
-    length *= 8 # Multiply the length by 8 because length is in units of bytes but we are working with bits (8bits=1byte)
+            length += 2 ** i
+    # Multiply the length by 8 because length is in units of bytes but we are working with bits (8 bits = 1 byte).
+    length *= 8
     
     msg = ''    # msg will store the message
-    c = 0       # this will store the decimal value of each byte
-    i = 0       # this will be our counter so we know when we've read an entire byte
+    x = 0       # column number
+    y = 1       # row number
+    i = 0       # bit number
+    bits = []   # list of total bits we get from the image
     
-    # These loops iterate through the pixels starting from the second row
-    for y in range(1,cols):
-        for x in range(rows):
-            r,g,b = pxls[x,y]   # get the values from the pixel
-            if ((r%2) == 1):    # Convert from binary to decimal one bit at a time
-                c += 2 ** i
-            length -= 1         # decrement length so we can check when the message is finished
-            i += 1              # keep track of bits
-            # if we've read an entire byte(8 total bits) we append the character to message and reset variables
-            if (i > 7):
-                msg = msg + chr(c)
-                c = 0
-                i = 0
-            # If we've read the entire message we return it
-            if (length == 0):
-                return msg
+    # For whatever reason a for loop wasn't working here so I created a while loop. TODO fix the for loop.
+    # This will continue until we have the correct number of bits from the image.
+    while (i < length):
+        r,g,b = pixels[x,y]
+        # Determine which color has the pixel we want based on i%3 and then set the correct value in bit.
+        if (i%3 == 0): bit = r%2
+        elif (i%3 == 1): bit = g%2
+        elif (i%3 == 2): bit = b%2
+        # Append our new bit to the list of bits and increment the counter.
+        bits.append(bit)
+        i += 1
+        # If we've gone through 3 bits we need to move to the next pixel (x += 1).
+        if (i%3 == 0 and i != 0):
+            x += 1
+            # If we've made it through an entire column we need to move to the next row (y += 1); Also reset x
+            if (x > rows):
+                x = 0
+                y += 1
 
-# main is just a simple menu function where the program starts. It's the UI.
-# Not gonna bother commenting at the moment, but I probably will later
+    # Loop through our list of bits, convert them to characters, and append them to the msg.
+    d = 0   # Store the decimal value of each byte.
+    b = 0   # The bit index Counter. We use it to know when we have a full byte.
+    for i,bit in enumerate(bits):
+        # Convert from binary to decimal one bit at a time.
+        d += (2**b) if (bit == 1) else 0
+        b += 1
+        # if we have all 8 bits add the character to the message and reset variables.
+        if (b == 8):
+            msg += chr(d)
+            d = 0
+            b = 0
+    return msg
+
+
+def print_help():
+    print("-------------------------------------")
+    print("|My Super Duper Steganography Script|")
+    print("-------------------------------------")
+    print("Usage: \n")
+    print('  (h)ide:    Hide a message')
+    print('  (u)nhide:  Unhide a message')
+    print('  (q)uit:    Stops the program')
+    print('  help:      Prints this help message')
+    print('  Example:\n     Command :-$ hide "message" img.jpg\n')
+
+
+def clear():
+    opsys = platform.system()
+    if (opsys == 'Linux'):
+        os.system('clear')
+    elif (opsys == 'Windows'):
+        os.system('cls')
+    else:
+        print '\n'*30 # Doing this always bugs me but hey, it works!
+
 def main():
-    userInput = 'who cares'
-    while (userInput != '0'):
-        print("1.) Hide a message")
-        print("2.) Unhide a message")
-        print("0.) quit")
-        userInput = raw_input("Command :-$ ")
-        if (userInput == '1'):
-            msg = open(raw_input("message file: ")).read()
-            imgName = raw_input("image name: ")
+    userInput = ''
+    print_help()
+    while (userInput != 'quit' and userInput != 'q'):
+        userInput = raw_input('Command :-$ ')
+        if (userInput == 'hide' or userInput == 'h'):
+            # Get the message.
+            msgFile = open(raw_input('  message file: '))
+            msg = msgFile.read()
+            msgFile.close()
+            # Get the image
+            imgName = raw_input('  image name: ')
             img = Image.open(imgName)
-            hideMsg(img, msg)
+
+            # Get the name for the output. For simplicity the output will always be a png file            
+            output = raw_input('  output name: ') + '.png'
+
+            # Hide the message, close the image, and have a wonderful day!
+            hide_msg(img, msg, output)
             img.close()
-        elif (userInput == '2'):
-            imgName = raw_input("image name: ")
+            print '\n[*] Message hidden in ' + output + '\n'
+        elif (userInput == 'unhide' or userInput == 'u'):
+            # Get the image to read from
+            imgName = raw_input('  image name: ')
             img = Image.open(imgName)
-            print ("[*] Begin Message:\n")
-            print readMsg(img)
-#            print ("[*] End Message\n")
-            raw_input("[*] End Message")
-        elif (userInput != '0'):
-            print ("Invalid Input")
-            userInput = 'who cares'
+            
+            # Read and print the message in a nice format
+            clear()
+            print ('[*] Begin Message:\n')
+            print read_msg(img)
+            img.close()
+            raw_input('[*] End Message')
+            clear()
+            print_help()
+        elif (userInput != 'quit' and userInput != 'q'):
+            print('[X] Unknown Command: ' + userInput)
+            print_help()
 
 main()
